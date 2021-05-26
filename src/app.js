10dd6ffd971163f4e689fd414a5bb6aca8c04031
Fabricio Saavedra <fabricio_inc27@hotmail.com>
2021-05-26 14:23:11 -0300

    added the basics for routing

--- src/app.js
+++ src/app.js
@@ -3,5 +3,40 @@ import ReactDOM from "react-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>Barebones</p>, document.querySelector("#app"));
+import {BrowserRouter, Route} from "react-router-dom"
+
+const ExpenseDashboardPage = ()=> (
+    <div>
+        This is the dashboad page
+    </div>
+);
+const AddExpensePage = () => (
+    <div>
+        This is the add expense page
+    </div>
+)
+const EditExpensePage = () => (
+    <div>
+        This is the edit expense page
+    </div>
+)
+const HelpPage = () => (
+    <div>
+        This is the help page
+    </div>
+)
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route exact={true} path="/" component={ExpenseDashboardPage} /> {/* if I remove the exact prop for this component,  every route starting with / will show this component also */}
+            <Route exact={true} path="/add" component={AddExpensePage} />
+            <Route exact={true} path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+        
+        {/* <Route />
+        <Route /> */}
+    </BrowserRouter>
+)
+ReactDOM.render(routes, document.querySelector("#app"));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-/* esto es un archivo de node, pero se parece bastante a js
+/* 
 para que wp funcione le tengo que dar entry point , donde arranca la app
 y output para archivo bundle
  */
@@ -18,35 +18,23 @@ module.exports = {
                 loader:"babel-loader",
                 options:{
                     presets:['@babel/preset-env', '@babel/preset-react'] // con esto no me hace falta el archivo .babelrc
-                    //plugins:['@babel/plugin-proposal-class-properties']
                 }
             },
             test:/\.js$/, // lo que tenga.js al final
             exclude: /node_modules/
         },{
-            test:/\.s?css$/i,
+            test:/\.s?css$/i, // lo que tenga .scss o .css al final
             use: ["style-loader","css-loader","sass-loader"],
             //estos 3 hacen que interprete scss como css y que el scss/css que hice, lo interprete como un style tag en head
         }]
     },
     devtool: 'eval-cheap-module-source-map',
     devServer:{
-        contentBase: path.join(__dirname,"public")
+        contentBase: path.join(__dirname,"public"),
+        historyApiFallback:true
     }
 };
 
-// ESTO PARA QUE ES?
-/* es para que no tenga que poner un monton de scripts tags en html 
-entre otras cosas, solo voy a necesitar uno para el js, para que use el
-bundle generado*/
-
-//hacer npm run build me da este output, si me tira un hash, se generó el bundle
-/* Hash: 4dc311afcdcbccbd4e6b
-Version: webpack 3.1.0
-Time: 63ms
-    Asset     Size  Chunks             Chunk Names
-bundle.js  2.64 kB       0  [emitted]  main
-   [0] ./src/app.js 168 bytes {0} [built] */
 
 // para decirle a webpack que use Babel, tengo que hacer un loader, algo que le diga como comportarse
 // tengo que instalar dos cosas, babel-core, parecido a babel-cli,y babel-loader
