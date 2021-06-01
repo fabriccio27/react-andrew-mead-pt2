b7a6ae2bd825a673d83ba0788f8fbf8c7d406ab8
fabriccio27 <fabricio_inc27@hotmail.com>
2021-06-01 19:16:05 -0300

    first commit of production build, separated css from js, changed source-map

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import {addExpense} from "./actions/expenses";
-import {setTextFilter} from "./actions/filters";
+//import {setTextFilter} from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
@@ -18,13 +18,13 @@ const store = configureStore();
 store.dispatch(addExpense({description:"Water bill", amount:1300, createdAt:15000}));
 store.dispatch(addExpense({description:"Gas bill", amount:1800, createdAt:20000}));
 store.dispatch(addExpense({description:"Rent", amount:109500, createdAt:15000}));
-console.log(store.getState());
-//store.dispatch(setTextFilter("water")); //esto en realidad lo quiero manejar a través de UI/UX
 
+//store.dispatch(setTextFilter("water")); //esto en realidad lo quiero manejar a través de UI/UX
+console.log("test");
 const state = store.getState();
 
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filter);
-console.log(visibleExpenses);
+
 
 /* con Provider, le puedo dar acceso directo al store a los distintos componentes que lo necesiten
 connect lo voy a usar en c/componente para poder leer del store o despachar acciones
--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,56 @@ para que wp funcione le tengo que dar entry point , donde arranca la app
 y output para archivo bundle
  */
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode:"development",
-    entry: './src/app.js',
-    //entry: './src/playground/hoc.js',
-    output:{
-        // path es absoluto, por eso uso __dirname
-        path:path.join(__dirname,"public"),
-        filename:'bundle.js'
-    },
-    module:{
-        rules:[{
-            use:{
-                loader:"babel-loader",
-                options:{
-                    presets:['@babel/preset-env', '@babel/preset-react'] // con esto no me hace falta el archivo .babelrc
-                }
-            },
-            test:/\.js$/, // lo que tenga.js al final
-            exclude: /node_modules/
-        },{
-            test:/\.s?css$/i, // lo que tenga .scss o .css al final
-            use: ["style-loader","css-loader","sass-loader"],
-            //estos 3 hacen que interprete scss como css y que el scss/css que hice, lo interprete como un style tag en head
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer:{
-        contentBase: path.join(__dirname,"public"),
-        historyApiFallback:true
-    }
-};
+module.exports = (env, argv) =>{
+    
+    const isProduction = argv.mode==="production";
+
+    return {
+        mode:"development",
+        entry: './src/app.js',
+        //entry: './src/playground/hoc.js',
+        output:{
+            // path es absoluto, por eso uso __dirname
+            path:path.join(__dirname,"public"),
+            filename:'bundle.js'
+        },
+        module:{
+            rules:[{
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:['@babel/preset-env', '@babel/preset-react'] // con esto no me hace falta el archivo .babelrc
+                    }
+                },
+                test:/\.js$/, // lo que tenga.js al final
+                exclude: /node_modules/
+            },{
+                test:/\.s?css$/i, // lo que tenga .scss o .css al final
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {loader:'css-loader', options:{sourceMap:true}},
+                    {loader:'sass-loader', options:{sourceMap:true}}
+                ],
+                //["style-loader","css-loader","sass-loader"],
+                //estos 3 hacen que interprete scss como css y que el scss/css que hice, lo interprete como un style tag en head
+            }]
+        },
+        plugins:[
+            new MiniCssExtractPlugin({ 
+                filename: 'styles.css'
+            })
+        ],
+        devtool: isProduction?'source-map':'inline-source-map',
+        devServer:{
+            contentBase: path.join(__dirname,"public"),
+            historyApiFallback:true
+        }
+    };
+}
+
+//module.exports = 
 
 
 // para decirle a webpack que use Babel, tengo que hacer un loader, algo que le diga como comportarse
