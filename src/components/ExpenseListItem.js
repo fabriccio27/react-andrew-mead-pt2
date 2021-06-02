ecc7e08a178dcb295a04024a8c78cc1bc0ac9f85
fabriccio27 <fabricio_inc27@hotmail.com>
2021-06-02 11:09:41 -0300

    added formatting through numeral and moment libraries

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 
 //aca en vez de usar props, podria usar un destructurado {dispatch(si lo requeria), id, description, amount, createdAt}
@@ -7,7 +9,11 @@ const ExpListItem = (props) => {
     return (
         <div>
             <h4><Link to={`/edit/${props.id}`}>{props.description}</Link></h4>
-            <p>Amount : {props.amount} | Created:{props.createdAt}</p>
+            <p>
+                {numeral(props.amount/100).format("$0,0.00")} 
+                - 
+                {moment(props.createdAt).format("MMMM Do, YYYY")}
+            </p>
         </div>
     );
 };
